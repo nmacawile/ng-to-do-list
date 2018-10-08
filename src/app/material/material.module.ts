07f6b2d4bd23560e71e44201f2c6bb13943f14cc
Mac <nmacawile@gmail.com>
2018-10-08 21:58:11 +0800

    Replace logout button with a dropdown menu button

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,7 @@ import {
   MatCheckboxModule,
   MatDatepickerModule,
   MatNativeDateModule,
+  MatMenuModule,
 } from '@angular/material';
 
 @NgModule({
@@ -26,6 +27,7 @@ import {
     MatCheckboxModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatMenuModule,
   ],
   declarations: [],
 })
