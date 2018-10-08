8fa37e180ceedd4236b3dda1842d12bd67ff9f4e
Mac <nmacawile@gmail.com>
2018-10-09 00:29:41 +0800

    Make the side nav responsive

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
+
+const SIDE_NAV_DRAWER = {
+  mode: 'over',
+  disableClose: false,
+  opened: false,
+};
+
+const SIDE_NAV_FIXED = {
+  mode: 'side',
+  disableClose: true,
+  opened: true,
+};
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.scss']
+  styleUrls: ['./project-list.component.scss'],
 })
 export class ProjectListComponent implements OnInit {
+  sideNav = SIDE_NAV_DRAWER;
 
-  constructor() { }
+  constructor(private media: ObservableMedia) {}
 
   ngOnInit() {
+    this.media.subscribe((mediaChange: MediaChange) => {
+      if (mediaChange.mqAlias === 'xs' || mediaChange.mqAlias === 'sm') {
+        this.sideNav = SIDE_NAV_DRAWER;
+      } else {
+        this.sideNav = SIDE_NAV_FIXED;
+      }
+    });
   }
-
 }
