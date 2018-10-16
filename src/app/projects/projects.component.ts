09bf3c0bb18371951fc05c0aa9d378bec41e5f08
Mac <nmacawile@gmail.com>
2018-10-16 20:53:43 +0800

    Close side nav when a nav link is clicked

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -4,26 +4,12 @@ import { ProjectsService } from './projects.service';
 import { Observable } from 'rxjs';
 import { Project } from '../project';
 
-const SIDE_NAV_DRAWER = {
-  mode: 'over',
-  disableClose: false,
-  opened: false,
-};
-
-const SIDE_NAV_FIXED = {
-  mode: 'side',
-  disableClose: true,
-  opened: true,
-};
-
 @Component({
   selector: 'projects-root',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
-  sideNav = { ...SIDE_NAV_DRAWER };
-
   projects$: Observable<Project[]>;
 
   constructor(
@@ -32,18 +18,10 @@ export class ProjectsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.updateSideNav();
-
-    this.media.subscribe(() => this.updateSideNav());
-
     this.projects$ = this.projectsService.getProjects();
   }
 
-  updateSideNav() {
-    if (!(this.media.isActive('xs') || this.media.isActive('sm'))) {
-      this.sideNav = { ...SIDE_NAV_FIXED };
-    } else {
-      this.sideNav = { ...SIDE_NAV_DRAWER };
-    }
+  get smallScreen() {
+    return this.media.isActive('xs') || this.media.isActive('sm');
   }
 }
